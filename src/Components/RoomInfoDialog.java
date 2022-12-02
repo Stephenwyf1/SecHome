baba451f4f741e2e7cc166ac8b6481ce58d69349
hatsukoi <hatsukoi.jiang@gmail.com>
2022-12-01 20:18:31 -0800

    add components package

---
+++ src/Components/RoomInfoDialog.java
@@ -0,0 +1,23 @@
+package Components;
+
+import javax.swing.*;
+import Backend.Room;
+
+public class RoomInfoDialog extends JDialog {
+    public RoomInfoDialog(Room room) {
+        JTextArea text = new JTextArea();
+        text.setLineWrap(true);
+        text.append("Location:" + room.getX() + "," + room.getY() + "\n");
+        if (room.getSensor() == null) {
+            text.append("Sensor Type: Not Installed\n");
+        } else {
+            text.append("Sensor Type:" + room.getSensor().getSensorType() + "\n");
+            text.append("Sensor State:" + room.getSensor().getState() + "\n");
+            text.append("Camera:" + (room.getSensor().getDeviceListener() == null? "NO" : "YES") + "\n");
+        }
+
+        this.setBounds(200,200,200,200);
+        this.add(text);
+        this.setVisible(true);
+    }
+}
